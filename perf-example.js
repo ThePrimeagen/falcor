7ceb449825783fa66fa4bbf1c4b3c940eb91e4a2
Michael Paulson <mpaulson@netflix.com>
2016-01-08 08:59:39 -0800

    altered the example to show how simple sites can change what happens.

--- perf-example.js
+++ perf-example.js
@@ -1,4 +1,8 @@
+var array = [1, 2, 3, 5, 7];
 module.exports = function a() {
-    var currentTime = Date.now();
-    while (Date.now() - currentTime < 5) { }
+    array = array.reduce(function(next, value) {
+        next.push(value + 3);
+        return next;
+    }, []);
+    return array.reduce(function(sum, value) { return sum + value; }, 0);
 };
